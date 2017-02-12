1092a5c4f4359f5e84c6c2b98da7e68e6171ebd2
chenag <23110134@qq.com>
2017-02-12 23:39:02 +0800

    m

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task("browser-sync", function () {
             baseDir: "./src" // 根路径。若根路径有index.html,就打开；没有的话，需要自己补充网址。
         },
         // browser: ["chrome", "firefox", "iexplore"], // 打开3个浏览器
-        browser: ["chrome"]
+        //browser: ["chrome"]
     })
 });
 
